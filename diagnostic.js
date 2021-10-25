204d031ee5d65388e672dcaa137ae76c3885334c
mr. whalen <michael.whalen@sitechhs.com>
2021-10-25 08:58:10 -0400

    Ready

---
+++ diagnostic.js
@@ -0,0 +1,9 @@
+//Q1) Identify the data type found in Swampert.js
+
+//Q2)Write a console log that references Swampert's base_stat "speed" value?
+
+//Q3) Write a function that iterates through Swampert's "Types" so that you ONLY print the type names in the console?
+
+//Q4) When using Fetch, what data type is  found after resolving the Promise?
+
+//Q5) Using Async Await, write a function that logs ONLY the ability names of the pokemon found at the following URL Endpoint "https://pokeapi.co/api/v2/pokemon/blaziken"
---
+++ swampert.js
@@ -0,0 +1,87 @@
+const Swampert = {
+  abilities: [
+    {
+      ability: {
+        name: "torrent",
+        url: "https://pokeapi.co/api/v2/ability/67/",
+      },
+      is_hidden: false,
+      slot: 1,
+    },
+    {
+      ability: {
+        name: "damp",
+        url: "https://pokeapi.co/api/v2/ability/6/",
+      },
+      is_hidden: true,
+      slot: 3,
+    },
+  ],
+  stats: [
+    {
+      base_stat: 100,
+      effort: 0,
+      stat: {
+        name: "hp",
+        url: "https://pokeapi.co/api/v2/stat/1/",
+      },
+    },
+    {
+      base_stat: 110,
+      effort: 3,
+      stat: {
+        name: "attack",
+        url: "https://pokeapi.co/api/v2/stat/2/",
+      },
+    },
+    {
+      base_stat: 90,
+      effort: 0,
+      stat: {
+        name: "defense",
+        url: "https://pokeapi.co/api/v2/stat/3/",
+      },
+    },
+    {
+      base_stat: 85,
+      effort: 0,
+      stat: {
+        name: "special-attack",
+        url: "https://pokeapi.co/api/v2/stat/4/",
+      },
+    },
+    {
+      base_stat: 90,
+      effort: 0,
+      stat: {
+        name: "special-defense",
+        url: "https://pokeapi.co/api/v2/stat/5/",
+      },
+    },
+    {
+      base_stat: 60,
+      effort: 0,
+      stat: {
+        name: "speed",
+        url: "https://pokeapi.co/api/v2/stat/6/",
+      },
+    },
+  ],
+  types: [
+    {
+      slot: 1,
+      type: {
+        name: "water",
+        url: "https://pokeapi.co/api/v2/type/11/",
+      },
+    },
+    {
+      slot: 2,
+      type: {
+        name: "ground",
+        url: "https://pokeapi.co/api/v2/type/5/",
+      },
+    },
+  ],
+  weight: 819,
+};
